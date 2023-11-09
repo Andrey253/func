a859ab1584712814bfcb6821e891f7ef90559665
Andrey253 <Andrey253@yandex.ru>
2023-11-09 15:46:16 +0700

    we

--- verification/lib/body_form.dart
+++ verification/lib/body_form.dart
@@ -1,239 +1,103 @@
-String htmlForm = '''<!doctype html>
+String form = '''<!doctype html>
 <html lang="en">
 
 <head>
-$scriptForm
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Forgot Password</title>
+    <!-- Font Awesome Icons  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
+
+    <!-- Google Fonts  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <title>Forgot Password UI Using CSS - @code.scientist x @codingtorque</title>
 </head>
 
-<body>
-    <style>
-   $styleForm
+<body> <style>
+     $STYLEfORM
     </style>
-$bodyDiv
+    <div class="card">
+        <p class="lock-icon"><i> class="fas-fa-lock"</i></p>
+        <h2>Forgot Password?</h2>
+        <p>You can reset your Password here</p>
+        <input type="text" class="passInput" placeholder="Email address">
+        <button>Send My Password</button>
+    </div>
 </body>
 
 </html>''';
-String bodyDiv = '''<div id="highlighted" class="hl-basic hidden-xs">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-sm-9 col-sm-offset-3 col-md-9 col-md-offset-3 col-lg-10 col-lg-offset-2">
-        <h1>Forgot Password</h1>
-      </div>
-    </div>
-  </div>
-</div>
-<div id="content" class="interior-page">
-  <div class="container-fluid">
-    <div class="row">
-      <!--Sidebar-->
-      <div class="col-sm-3 col-md-3 col-lg-2 sidebar equal-height interior-page-nav hidden-xs">
-        <div class="dynamicDiv panel-group" id="dd.0.1.0">
-          <div id="subMenu" class="panel panel-default">
-            <ul class="subMenuHighlight panel-heading">
-              <li class="subMenuHighlight panel-title" id="subMenuHighlight">
-                <a id="li_291" class="subMenuHighlight" href=""><span>Register</span></a>
-              </li>
-            </ul>
-            <ul class="panel-heading">
-              <li class="panel-title">
-                <a class="subMenu1" href=""><span class="subMenuHighlight">Forgot Password</span></a>
-              </li>
-            </ul>
-            <ul class="panel-heading">
-              <li class="panel-title">
-                <a class="subMenu1" href=""><span>Login</span></a>
-              </li>
-            </ul>
-          </div>
-          <div class="item item-nopad item-noborder item-gold">
-            <a style="padding: 5% 0px;" href="" class="btn btn-primary btn-block" role="button">LEARN MORE</a> 
-          </div>
-        </div>
-      </div>
+String STYLEfORM = '''* {
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+}
 
-      <!--Content-->
-      <div class="col-sm-9 col-md-9 col-lg-10 content equal-height">
-        <div class="content-area-right">
-          <div class="content-crumb-div">
-            <a href="">Home</a> | <a href="">Your Account</a> | Forgot Password
-          </div>
-          <div class="row">
-            <div class="col-md-5 forgot-form">
-              <p>Please enter your email address below and we will send you information to change your password.</p>
-              <label class="label-default" for="un">Email Address</label> <input id="email_addy" name="email_addy" class="form-control" type="text"><br>
-              <a id="mybad" class="btn btn-primary" role="button">I FORGOT</a>
-            </div>
-            <div class="col-md-5 forgot-return" style="display:none;">
-              <h2>Reset Password Sent</h2>
-              <p>An email has been sent to your address with a reset password you can use to access your account.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>''';
-String styleForm = '''#highlighted {
-  position: relative;
-  background-color: #dc143c;
+body {
+    background-color: #ff99f5;
+    background-image:
+        radial-gradient(at 61% 4%, hsla(303, 91%, 61%, 1) 0px, transparent 50%),
+        radial-gradient(at 75% 66%, hsla(196, 91%, 79%, 1) 0px, transparent 50%),
+        radial-gradient(at 98% 88%, hsla(76, 87%, 78%, 1) 0px, transparent 50%),
+        radial-gradient(at 23% 16%, hsla(238, 96%, 77%, 1) 0px, transparent 50%),
+        radial-gradient(at 95% 65%, hsla(13, 91%, 75%, 1) 0px, transparent 50%),
+        radial-gradient(at 10% 79%, hsla(228, 96%, 69%, 1) 0px, transparent 50%),
+        radial-gradient(at 85% 58%, hsla(328, 81%, 68%, 1) 0px, transparent 50%);
+    background-repeat: no-repeat;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 15rem 0;
 }
-#highlighted .container-fluid h1, 
-#highlighted .container-fluid p {
-  color: #fff;
+
+.card {
+    backdrop-filter: blur(16px) saturate(180%);
+    -webkit-backdrop-filter: blur(16px) saturate(180%);
+    background-color: rgba(0, 0, 0, 0.75);
+    border-radius: 12px;
+    border: 1px solid rgba(255, 255, 255, 0.125);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 30px 40px;
 }
-#highlighted .container-fluid h1 {
-  font-size: 54px;
-  font-family: Verlag,museo-sans,'Helvetica Neue',Helvetica,Arial,sans-serif;
-  color: #414141;
-  font-weight: 300;
+
+.lock-icon {
+    font-size: 3rem;
 }
-#content {
-  background-position: right bottom;
-  background-repeat: no-repeat;
+
+h2 {
+    font-size: 1.5rem;
+    margin-top: 10px;
+    text-transform: uppercase;
 }
-.interior-page {
-  background-color: #FFF;
-  padding-bottom: 30px;
+
+p {
+    font-size: 12px;
 }
-#highlighted+#content.interior-page .interior-page-nav {
-  margin-top: -4em;
+
+.passInput {
+    margin-top: 15px;
+    width: 80%;
+    background: transparent;
+    border: none;
+    border-bottom: 2px solid deepskyblue;
+    font-size: 15px;
+    color: white;
+    outline: none;
 }
-#highlighted+#content.interior-page .interior-page-nav, 
-.interior-page .interior-page-nav {
-  padding-left: 0;
-}
-.sidebar {
-  margin-top: 2em;
-}
-.content-area-right {
-  max-width: 1200px;
-  padding-right: 15px;
-  padding-left: 15px;
-}
-.container-fluid>.row h2.crumb-title {
-  margin-bottom: 0;
-}
-.page-title {
-  min-height: 50px;
-}
-.page-title, ul {
-  margin: 0;
-  list-style: none;
-}
-.content-crumb-div {
-  margin: 5px 0 20px;
-}
-a {
-  text-decoration: none;
-}
-.container-fluid .row .modal, 
-.page .modal {
-  position: fixed;
-  top: 35%;
-}
-#highlighted+#content.interior-page .interior-page-nav, 
-.interior-page .interior-page-nav {
-  padding-left: 0;
-}
-#highlighted+#content.interior-page .interior-page-nav {
-  margin-top: -4em;
-}
-.dynamicDiv.panel-group {
-  border: 1px solid #E7E9E9;
-  margin-left: 30px;
-}
-.panel-group {
-  margin-bottom: 0;
-  background-color: #fff;
-}
-.panel-group .panel {
-  -webkit-border-radius: 0;
-  -moz-border-radius: 0;
-  border-radius: 0;
-  border: none;
-  box-shadow: none;
-}
-.panel-group .panel-heading {
-  padding: 0;
-  border: none;
-}
-.panel-default>.panel-heading {
-  color: #333;
-  background-color: #f5f5f5;
-  border-color: #ddd;
-}
-.panel-group .panel-heading .panel-title {
-  font-size: 1.1em;
-  font-family: Verlag,museo-sans,'Helvetica Neue',Helvetica,Arial,sans-serif;
-}
-.interior-page-nav .panel-group .panel-heading .panel-title a {
-  background: 0 0;
-}
-.panel-group .panel-heading .panel-title a {
-  display: block;
-  padding: 15px 45px 15px 15px;
-  background: url(/resources/images/misc/icon_accordion-open.png) 95% center no-repeat #f6f6f6;
-}
-span.subMenuHighlight, 
-ul.panel-heading li.panel-title a:hover {
-  color: #ed3c95;
-}
-.panel-group .panel-heading .panel-title {
-  font-size: 1.1em;
-  font-family: Verlag,museo-sans,'Helvetica Neue',Helvetica,Arial,sans-serif;
-}
-ul.panel-heading {
-  margin-bottom: 1px;
-}
-.panel-group .panel-heading .panel-title a {
-  display: block;
-  padding: 15px 45px 15px 15px;
-  background: url(/resources/images/misc/icon_accordion-open.png) 95% center no-repeat #f6f6f6;
-}
-.panel-group {
-  margin-bottom: 0;
-  background-color: #fff;
-}
-.label-default {
-  background-color: #fff;
-  margin-top: 10px;
-}
-label {
-  display: inline-block;
-  margin-bottom: 5px;
-  font-weight: 700;
-}
-.form-control {
-  border-radius: 0;
-}
-.btn-primary {
-  color: #fff;
-  background-color: #dc143c;
-  border-color: #ea3e10;
-  width: 100%;
-}
-.btn-block {
-  display: block;
-}
-.btn {
-  padding: 8px 28px;
-  font-weight: 400;
-  -webkit-transition: background .3s ease-in;
-  transition: background .3s ease-in;
-  white-space: normal;
-  border-width: 0 0 1px;
-}
-.content-area-right {
-  margin-top: 10px;
-}
-@media (min-width: 992px)
-  #highlighted .container-fluid {
-    margin-bottom: 2.5rem;
-  }
+
+button {
+    margin-top: 15px;
+    width: 80%;
+    background-color: deepskyblue;
+    color: white;
+    padding: 10px;
+    text-transform: uppercase;
 }''';
-String scriptForm =
-    '''<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>''';
