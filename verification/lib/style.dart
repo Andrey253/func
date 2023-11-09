fd8747363b415175b6586dee2c890b5ce3fe5983
Andrey <andrey253@yandex.ru>
2023-11-09 19:38:26 +0700

    23

--- verification/lib/style.dart
+++ verification/lib/style.dart
@@ -1,37 +1,71 @@
-const style = '''a.button17 {
-  display: inline-block;
-  font-family: arial,sans-serif;
-  font-size: 11px;
-  color: rgb(205,216,228);
-  text-shadow: 0 -1px rgb(46,53,58);
-  text-decoration: none;
-  user-select: none;
-  line-height: 2em;
-  padding: 1px 1.2em;
+const style = '''* {
+  margin: 0;
+  padding: 0;
+  font-family: 'Poppins', sans-serif;
+}
+
+body {
+  background-color: #ff99f5;
+  background-image:
+      radial-gradient(at 61% 4%, hsla(303, 91%, 61%, 1) 0px, transparent 50%),
+      radial-gradient(at 75% 66%, hsla(196, 91%, 79%, 1) 0px, transparent 50%),
+      radial-gradient(at 98% 88%, hsla(76, 87%, 78%, 1) 0px, transparent 50%),
+      radial-gradient(at 23% 16%, hsla(238, 96%, 77%, 1) 0px, transparent 50%),
+      radial-gradient(at 95% 65%, hsla(13, 91%, 75%, 1) 0px, transparent 50%),
+      radial-gradient(at 10% 79%, hsla(228, 96%, 69%, 1) 0px, transparent 50%),
+      radial-gradient(at 85% 58%, hsla(328, 81%, 68%, 1) 0px, transparent 50%);
+  background-repeat: no-repeat;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 15rem 0;
+}
+
+.card {
+  backdrop-filter: blur(16px) saturate(180%);
+  -webkit-backdrop-filter: blur(16px) saturate(180%);
+  background-color: rgba(0, 0, 0, 0.75);
+  border-radius: 12px;
+  border: 1px solid rgba(255, 255, 255, 0.125);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px 40px;
+}
+
+.lock-icon {
+  font-size: 3rem;
+}
+
+h2 {
+  font-size: 1.5rem;
+  margin-top: 10px;
+  text-transform: uppercase;
+}
+
+p {
+  font-size: 12px;
+}
+
+.passInput {
+  margin-top: 15px;
+  width: 80%;
+  background: transparent;
+  border: none;
+  border-bottom: 2px solid deepskyblue;
+  font-size: 15px;
+  color: white;
   outline: none;
-  border: 1px solid rgba(33,43,52,1);
-  border-radius: 3px;
-  background: rgb(81,92,102) linear-gradient(rgb(81,92,102), rgb(69,78,87));
-  box-shadow:
-   inset 0 1px rgba(101,114,126,1),
-   inset 0 0 1px rgba(140,150,170,.8),
-   0 1px rgb(83,94,104),
-   0 0 1px rgb(86,96,106);
 }
-a.button17:active {
-  box-shadow:
-   inset 0 1px 3px rgba(0,10,20,.5),
-   0 1px rgb(83,94,104),
-   0 0 1px rgb(86,96,106);
+
+button {
+  margin-top: 15px;
+  width: 80%;
+  background-color: deepskyblue;
+  color: white;
+  padding: 10px;
+  text-transform: uppercase;
 }
-a.button17:focus:not(:active) {
-  border: 1px solid rgb(22,32,43);
-  border-bottom: 1px solid rgb(25,34,45);
-  background: rgb(53,61,71);
-  box-shadow:
-   inset 0 1px 3px rgba(0,10,20,.5),
-   0 1px rgb(83,94,104),
-   0 0 1px rgb(86,96,106);
-  pointer-events: none;
-}
+
 ''';
