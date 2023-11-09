5879efd5dbaf61e00a83b6c19307a3aaac7635de
Andrey253 <Andrey253@yandex.ru>
2023-11-09 11:03:24 +0700

    34

--- emailverification/lib/html.dart
+++ emailverification/lib/html.dart
@@ -1,1 +1,15 @@
-String html = '<!DOCTYPE html><html>  <head>    <meta charset="utf-8">      <title>        Подтверждение регистрации      </title>    </head>  <body>    <style>      {style}    </style>    {body}  </body></html>';
+const html = '''<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+      <title>
+        Подтверждение регистрации
+      </title>
+    </head>
+  <body>
+    <style>
+      {style}
+    </style>
+    {body}
+  </body>
+</html>''';
