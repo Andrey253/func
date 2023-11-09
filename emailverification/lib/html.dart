803d8f6cd8b7c7ff7784836134cac4f8d797263f
Andrey253 <Andrey253@yandex.ru>
2023-11-09 10:56:24 +0700

    e

--- emailverification/lib/html.dart
+++ emailverification/lib/html.dart
@@ -1,15 +1,1 @@
-String html = '''<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-      <title>
-        Подтверждение регистрации
-      </title>
-    </head>
-  <body>
-    <style>
-      {style}
-    </style>
-    {body}
-  </body>
-</html>''';
+String html = '<!DOCTYPE html><html>  <head>    <meta charset="utf-8">      <title>        Подтверждение регистрации      </title>    </head>  <body>    <style>      {style}    </style>    {body}  </body></html>';
