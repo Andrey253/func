01d47ff7edc4a310ef4650dd1a329198433f704c
Andrey253 <Andrey253@yandex.ru>
2023-11-09 14:00:00 +0700

    2

---
+++ resetpassword/lib/html.dart
@@ -0,0 +1,16 @@
+const html = '''<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+      <title>
+        Подтверждение регистрации
+      </title>
+    </head>
+  <body>
+    <style>
+      {style}
+    </style>
+    <h1>{h1}</h1>
+    {body}
+  </body>
+</html>''';
