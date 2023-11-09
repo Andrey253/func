260e2be454077633dbb64c4a9331803112cb7615
Andrey253 <Andrey253@yandex.ru>
2023-11-09 14:42:27 +0700

    5

--- verification/lib/body.dart
+++ verification/lib/body.dart
@@ -2,7 +2,7 @@ import 'package:dart_appwrite/models.dart';
 
 String body(Token? token) => token == null
     ? '''
-<a href="#" class="button17" tabindex="0" onclick="window.top.close();">
+<a href="https://andrey253.github.io" class="button17" tabindex="0" >
     Ошибка верификации. Закрыть окно.
 </a>'''
     : '''
